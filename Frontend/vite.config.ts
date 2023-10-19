7a819db8b77aea38db6f1b4d119d7e26ee2f4423
Leonel Angel <leonelgo66@gmail.com>
2023-10-19 12:38:54 -0300

    feat: add alias and normalize.css

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true
+  },
+  resolve: {
+    alias: {
+      '@pages': path.resolve(__dirname, './src/pages')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import 'src/styles/index.css';`
+      }
+    }
+  }
+});
