4066c95c5fcc9f43fec5f9c85f7958740fab3b80
Leonel Angel <leonelgo66@gmail.com>
2023-10-22 18:24:16 -0300

    feat: install testing-reack-hooks

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
     alias: {
       '@pages': path.resolve(__dirname, './src/pages'),
       '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets')
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@hooks': path.resolve(__dirname, './src/hooks')
     }
   },
   css: {
